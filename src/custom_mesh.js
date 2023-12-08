4fbdc4e7e1a06b12170e1542328d032b803a7310
Tomas_Mena <tomasmena@gmail.com>
2023-12-08 13:13:51 +0100

    Last version colors files

--- src/custom_mesh.js
+++
@@ -1,8 +0,0 @@
-//import data from '../assets/midpoints.json' assert {type:'JSON'}
-
-async function panels(){
-    const response = fetch('./assets/panels.json');
-    return (await response).json()
-};
-export const panelsr =  await panels();
-
